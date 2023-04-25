a8fcc31489e0cd9d4f10c5de200aee65e95578be
[SW2/서울/2반] <dayoon1124@naver.com>
2023-04-26 02:59:34 +0900

    [DEL] 필요없는 파일 삭제

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Weather from './components/Weather';
+import Weather2 from './components/Weather2';
+import SearchMap from './components/SearchMap';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   return (
+      <div className="App">
+         {/* <Weather /> */}
+         <Weather2 />
+         <SearchMap />
+      </div>
+   );
 }
 
 export default App;
